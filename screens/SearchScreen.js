808b42834371a93d3c7aa828d86b214a2c8a1f91
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-11 19:52:09 +0100

    add ShowResults components on SearchSreen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,12 +2,12 @@ import React, {useState,useEffect} from 'react';
 import { Text, View, StyleSheet } from 'react-native'; 
 import SearchBar from '../components/SearchBar';
 import useResult from '../hooks/useResult';
-
+import ShowResults from '../components/ShowResults'
 const INITIALSEARCH = "Asian food"
 
 const SearchScreen = () => {
     const [term, setTerm] = useState();
-    const [results, errorMessage, searchAPI] = useResult();
+    const [results, errorMessage, searchAPI] = useResult(INITIALSEARCH);
     console.log(results)
 
     return <View style = {styles.View }>
@@ -19,6 +19,8 @@ const SearchScreen = () => {
             />
         {errorMessage ? <Text>{errorMessage}</Text>:null}
         <Text> We have found {results.length} results</Text>
+        <ShowResults title = "Cost Efficient"/>
+
     </View>;
 }
 

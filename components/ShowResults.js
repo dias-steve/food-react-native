ac25940eed6d3108ce6b2f9c4899d14dd1464e61
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-11 19:45:29 +0100

    Creating of ShowResult Compoments

---
+++ components/ShowResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const ShowResult = ({title}) => {
+    return <View>
+        <Text>{title}</Text>
+    </View>;
+}
+
+const styles = StyleSheet.create({
+
+});
+
+return ShowResult;

cbb5491a3c0ddf85ceea1a020b07f08c51cbe03a
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-11 19:56:56 +0100

    styling of the title of Show results

--- components/ShowResults.js
+++ components/ShowResults.js
@@ -3,12 +3,15 @@ import {Text, View, StyleSheet} from 'react-native';
 
 const ShowResult = ({title}) => {
     return <View>
-        <Text>{title}</Text>
+        <Text style = {styles.titleStyle}>{title}</Text>
     </View>;
 }
 
 const styles = StyleSheet.create({
-
+    titleStyle: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
 });
 
 export default ShowResult;

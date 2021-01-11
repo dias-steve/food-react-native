4a95680ff047988bf6edfb2a5fbed818ff183447
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-11 21:05:03 +0100

    adding flatlist on Show result

--- components/ShowResults.js
+++ components/ShowResults.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
 
 const ShowResult = ({title, results}) => {
 
     return <View>
         <Text style = {styles.titleStyle}> {title} </Text>
         <Text> {results.length}</Text>
+        <FlatList
+            horizontal
+            keyExtractor = {results => results.id}
+            data = {results}
+            renderItem = {({item})=>{
+                return <Text> {item.name}</Text>;
+            }}
+        />
     </View>; 
 }
 

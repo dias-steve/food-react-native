205cafeab52aac8e3c7c84b46dc45359aa4e394e
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-12 13:38:15 +0100

    Creating of de ResultDetail component

---
+++ components/ResultDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const ResultDetail =() =>{
+    return <Text>Results Detail</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ResultDetail;

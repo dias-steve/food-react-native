1f6a0e512d90efcc694923376f606e53f6727a7a
Steve DIAS <steve@MacBook-Pro-de-Steve.local>
2021-01-12 13:59:55 +0100

    adding image, rating, review_count data on Showing ResultDetail component

--- components/ResultDetail.js
+++ components/ResultDetail.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 const ResultDetail =({result}) =>{
-    return <Text> {result.name}</Text>;
+    return <View> 
+        <Image 
+            source={{uri: result.image_url}}
+            style = {styles.imageStyle}
+        />
+        <Text style = {styles.name}> {result.name}</Text>
+        <Text>{result.rating} Starts, {result.review_count} Reviews</Text>
+    </View>;
 };
 
 const styles = StyleSheet.create({
+    imageStyle: {
+        width: 250,
+        height: 120,
+        borderRadius: 5
 
+    },
+    name:{
+        fontWeight: 'bold',
+        fontSize: 15
+    }
 });
 
 export default ResultDetail;
